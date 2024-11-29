9ed72711ff5608e9f656adcccf03b30acd2b1d8c
wgreine2 <144729039+Will-GreinerUWYO@users.noreply.github.com>
2024-11-28 21:47:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function insertionSortReverse(array) {
-    for(var i = array.length - 1; i > 0; i--) {
+    for(var i = array.length - 1; i >= 0; i--) {
         var val = array[i];
         var j;
         for(j = i; j < array.length - 1 && array[j+1] < val; j++) {
